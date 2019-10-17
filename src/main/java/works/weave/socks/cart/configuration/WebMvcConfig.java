1412971229f626ee3d8fa4652c8078f758590ca2
Florian Bacher <florian.bacher@dynatrace.com>
2019-10-17 13:24:38 +0200

    #950 added interceptor

--- src/main/java/works/weave/socks/cart/configuration/WebMvcConfig.java
+++ src/main/java/works/weave/socks/cart/configuration/WebMvcConfig.java
@@ -1,17 +1,20 @@
 package works.weave.socks.cart.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import works.weave.socks.cart.middleware.RequestCounterInterceptor;
 //import works.weave.socks.cart.middleware.HTTPMonitoringInterceptor;
 
 @Configuration
 public class WebMvcConfig {
-    // @Bean
-    // HTTPMonitoringInterceptor httpMonitoringInterceptor() {
-    //     return new HTTPMonitoringInterceptor();
-    // }
+	@Autowired
+	private RequestCounterInterceptor requestCounterInterceptor;
 
-    // @Bean
-    // public MappedInterceptor myMappedInterceptor(HTTPMonitoringInterceptor interceptor) {
-    //     return new MappedInterceptor(new String[]{"/**"}, interceptor);
-    // }
+	//@Autowired
+	//private RequestTimingInterceptor requestTimingInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(requestCounterInterceptor);
+		//registry.addInterceptor(requestTimingInterceptor);
+	}
 }
