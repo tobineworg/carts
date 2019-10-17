ab979caba49330b407a7f2c2ebde788cfac1bf4e
Florian Bacher <florian.bacher@dynatrace.com>
2019-10-17 12:34:33 +0200

    #950 enable spring metrics

--- src/main/java/works/weave/socks/cart/CartApplication.java
+++ src/main/java/works/weave/socks/cart/CartApplication.java
@@ -2,8 +2,10 @@ package works.weave.socks.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.metrics.export.prometheus.EnablePrometheusMetrics;
 
 @SpringBootApplication
+@EnablePrometheusMetrics
 public class CartApplication {
     public static void main(String[] args) {
         SpringApplication.run(CartApplication.class, args);
