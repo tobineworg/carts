e158bdd6738057a9aba5b5404910e0bd5fed89a4
Florian Bacher <florian.bacher@dynatrace.com>
2019-10-17 14:59:00 +0200

    #950 added interceptor

--- src/main/java/works/weave/socks/cart/configuration/WebMvcConfig.java
+++ src/main/java/works/weave/socks/cart/configuration/WebMvcConfig.java
@@ -1,17 +1,11 @@
 package works.weave.socks.cart.configuration;
 
-import com.example.prometheus_demo.interceptor.RequestCounterInterceptor;
 //import com.example.prometheus_demo.interceptor.RequestTimingInterceptor;
-import io.prometheus.client.Collector;
-import io.prometheus.client.hibernate.HibernateStatisticsCollector;
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import org.springframework.context.annotation.Configuration;
 import works.weave.socks.cart.middleware.RequestCounterInterceptor;
 //import works.weave.socks.cart.middleware.HTTPMonitoringInterceptor;
